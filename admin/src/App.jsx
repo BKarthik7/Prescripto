82949aa95a861e1e583c6659f1cafdf9930e582e
B Karthik <bangikarthik7@gmail.com>
2024-12-29 01:41:31 +0530

    feat: pages outline

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,22 @@
-import React, {useContext} from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
+import React, { useContext } from 'react'
+import { AdminContext } from './context/AdminContext';
+import { Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { AdminContext } from './context/AdminContext';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
+import Login from './pages/Login';
+import DoctorAppointments from './pages/Doctor/DoctorAppointments';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard';
+import DoctorProfile from './pages/Doctor/DoctorProfile';
 
 const App = () => {
 
-  const {aToken} = useContext(AdminContext)
+  const { aToken } = useContext(AdminContext)
 
   return aToken ? (
     <div className='bg-[#F8F9FD]'>
@@ -19,13 +26,20 @@ const App = () => {
         <Sidebar />
         <Routes>
           <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointments />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
         </Routes>
       </div>
     </div>
   ) : (
     <>
-      <Login />
       <ToastContainer />
+      <Login />
     </>
   )
 }
