0640b10f7e9dc27c4988b1f61c98d410a0b4fbe3
B Karthik <bangikarthik7@gmail.com>
2024-12-29 03:32:26 +0530

    feat: route configuration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express"
+import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
-import adminRouter from './routes/adminRoute.js';
+import userRouter from "./routes/userRoute.js"
+import doctorRouter from "./routes/doctorRoute.js"
+import adminRouter from "./routes/adminRoute.js"
 
 // app config
 const app = express()
@@ -16,7 +18,9 @@ app.use(express.json())
 app.use(cors())
 
 // api endpoints
-app.use('/api/admin',  adminRouter)
+app.use("/api/user", userRouter)
+app.use("/api/admin", adminRouter)
+app.use("/api/doctor", doctorRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working")
