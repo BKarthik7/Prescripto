5d935b8672851df414fed90f5904c55453fe2ca5
B Karthik <bangikarthik7@gmail.com>
2024-12-29 03:17:01 +0530

    feat: Admin APIs

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -103,4 +103,22 @@ const addDoctor = async (req, res) => {
 		res.json({ success: false, message: error.message });
 	}
 };
-export { loginAdmin, addDoctor };
+
+// API to get all doctors list for admin panel
+const allDoctors = async (req, res) => {
+	try {
+
+			const doctors = await doctorModel.find({}).select('-password')
+			res.json({ success: true, doctors })
+
+	} catch (error) {
+			console.log(error)
+			res.json({ success: false, message: error.message })
+	}
+}
+
+export { 
+	loginAdmin, 
+	addDoctor,
+	allDoctors
+};
